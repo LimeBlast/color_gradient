1f8078b55f4fc1f7ec865bc990cacadb9a18ee49
Daniel Hollands <daniel@limeblast.co.uk>
2016-04-27 19:49:35 +0100

    Implement result_array method

--- lib/color_gradient.rb
+++ lib/color_gradient.rb
@@ -6,21 +6,30 @@ class ColorGradient
     raise ArgumentError, 'stop Argument is not Color gem object' unless stop.is_a? Color
     raise ArgumentError, 'resolution Argument is not integer' unless resolution.is_a? Integer
 
-    @start      = start
-    @stop       = stop
-    @resolution = Float(resolution)
+    @start        = start
+    @stop         = stop
+    @resolution   = Float(resolution)
+    @result_array = []
+
+    (resolution+1).times do |i|
+      @result_array << generate_step(i)
+    end
   end
 
   def gradient(step)
+    result_array[step]
+  end
+
+  private
+
+  attr_accessor :start, :stop, :resolution, :result_array
+
+  def generate_step(step)
     r = interpolate(start.red.to_i, stop.red.to_i, step)
     g = interpolate(start.green.to_i, stop.green.to_i, step)
     b = interpolate(start.blue.to_i, stop.blue.to_i, step)
     Color::RGB.new(r, g, b)
   end
-
-  private
-
-  attr_accessor :start, :stop, :resolution
 
   def interpolate(start, stop, step)
     if start < stop
